d950cba9a952b08fb963cea75a67b4b4a681cbd0
AgusOrq <disciplinam.studium@gmail.com>
2026-06-13 15:36:16 -0300

    cambio de texto

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def saludo(nombre):
 
 @app.route("/")
 def home():
-    return saludo("Profe")
+    return saludo("Profe Minoli")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
