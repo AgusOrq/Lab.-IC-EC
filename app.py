c810aeca27a128e9c69330e796aac42e11b31f46
AgusOrq <disciplinam.studium@gmail.com>
2026-06-18 19:01:30 -0300

    listo 2.0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def saludo(nombre):
 
 @app.route("/")
 def home():
-    return saludo("Mundo!")
+    return saludo("Version 2.0")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
